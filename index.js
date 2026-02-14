221d0058a9173d111ed9b6734f3f0635746d3a8e
Aernixis <Aernatas@gmail.com>
2026-02-13 19:26:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ async function updatePanel(channel) {
   const info = new EmbedBuilder()
     .setTitle('KOS Submission System')
     .setColor(0xFF0000)
-    .setDescription(
+    .setDescription('
 **KOS Submission System**
 This bot organizes LBG players and clans onto the KOS list for YX members.
 **If there are multiple users with the same display name on the KOS list, a username will be required.**
@@ -369,5 +369,6 @@ require('http').createServer((req, res) => res.end('Bot running')).listen(PORT);
 
 /* ===================== LOGIN ===================== */
 client.login(process.env.TOKEN);
+
 
 
